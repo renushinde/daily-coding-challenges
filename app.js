91cd10a26e0ef1e33c9e0da93809dfd8984f883d
Renu <renunikhilp@gmail.com>
2022-08-19 12:09:52 -0700

    problem 4, 5, 6

--- app.js
+++ app.js
@@ -50,3 +50,54 @@ function addTwoNumbers(num1, num2) {
   }
 }
 console.log(addTwoNumbers(-10, 2));
+
+/*-----------------------------------------------------------------------------
+Challenge: 03-sumNumbers
+Difficulty: Basic
+Prompt:
+- Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
+- If the array is empty, return 0 (zero).
+Examples:
+sumNumbers([10]) //=> 10
+sumNumbers([5, 10]) //=> 15
+sumNumbers([2, 10, -5]) //=> 7
+sumNumbers([]) //=> 0
+-----------------------------------------------------------------------------*/
+// Your solution for 03-sumNumbers here:
+
+// function sumNumbers(arr) {
+//   var total = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+// }
+// console.log(sumNumbers[(100, 5)]);
+
+function sumNumbers(array) {
+  let total = 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+console.log(sumNumbers([]));
+
+// Challenge: 06-range
+// Difficulty: basic
+// Prompt:
+// - Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.
+// - The range function must be called with the first argument less than or equal to the second argument; otherwise, return the string "First argument must be less than second".
+
+function range(a, b) {
+  let rangeArr = [];
+  for (let i = a; i <= b; i++) {
+    rangeArr.push(i);
+  }
+  return rangeArr;
+}
+
+console.log(range(7, 10));
